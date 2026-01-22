33012b0eaea1723f60551b0a2b86d29346042fc9
sujeet-crossml <sujeet.shah@crossml.com>
2026-01-22 12:44:18 +0530

    Subject: Changed Project structure for good files management and Upgrade the config db file.
    Reasons:
    I have made this change so that it follow the go project structure convention. Also database code is now Performance Tunned.
    
    Features/Fixed:
    - Fixed ConnectDB function for the database configration
    - Improved the performance tuning of database.

---
+++ internal/config/database.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func ConnectDB(){
+	dsn := "root:password@tcp(localhost:3306)/shop?parseTime=true"
+	DB, err := sql.Open("mysql", dsn)	
+	if err != nil{
+		log.Fatal("Error opening DB: ",err)
+	}
+
+	DB.SetMaxOpenConns(25)
+	DB.SetMaxIdleConns(25)
+	DB.SetConnMaxLifetime(5*time.Minute)
+
+	if err := DB.Ping(); err != nil{
+		log.Fatal("Error conning to DB: ",err)
+	}
+	
+	fmt.Println("Database connection established")
+
+}
